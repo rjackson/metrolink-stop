3775cdcfea89dc0d6d01682fbc491de5ff4767e5
Rob Jackson <rob@rjackson.me>
2021-07-24 00:40:01 +0100

    clear out junk

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/icon.svg" />
+        <title>Metrolink stop, doot doot</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
