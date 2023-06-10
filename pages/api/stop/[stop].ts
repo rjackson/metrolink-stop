e679c79dffa75e33fb9372dd9fae49872dce77b8
Rob Jackson <rob@rjackson.me>
2023-06-10 12:31:26 +0100

    Fix TypeError on api/stop/[stop].ts

--- pages/api/stop/[stop].ts
+++ pages/api/stop/[stop].ts
@@ -5,6 +5,11 @@ const Stop = async (req: NextApiRequest, res: NextApiResponse) => {
   const { stop: rawStop } = req.query;
   const stop = Array.isArray(rawStop) ? rawStop[0] : rawStop;
 
+  if (!stop) {
+    res.status(404).json({ error: "Stop not found" });
+    return;
+  }
+
   try {
     res.status(200).json(await getStopInfo(stop));
   } catch (err) {
