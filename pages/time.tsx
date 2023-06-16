e705f8205b59f8cfbb29f6018876edd710de2650
Rob Jackson <rob@rjackson.me>
2023-06-16 18:06:19 +0100

    Adjust isochrone colours

--- pages/time.tsx
+++ pages/time.tsx
@@ -13,35 +13,58 @@ type MapProps = {
 };
 
 const initClientOnlyMap = async (): Promise<(props: MapProps) => JSX.Element> => {
-  
   const { MapContainer, GeoJSON, CircleMarker } = await import("react-leaflet");
   const { VectorBasemapLayer } = await import("../components/leaflet/VectorBasemapLayer");
-  
+
   const center: LatLngTuple = [53.4781, -2.2433]; // St Peters Square
   const zoom = 10;
-  
+
   const ClientOnlyMap = ({ isochrones }: MapProps) => {
     const prefersDark = usePrefersDark();
+
+    const durationColors = {
+      "10": `hsl(60, 100%, ${prefersDark ? 50 : 50}%)`,
+      "20": `hsl(60, 100%, ${prefersDark ? 33.4 : 67}%)`,
+      "30": `hsl(60, 100%, ${prefersDark ? 17 : 84}%)`,
+      "40": `hsl(60, 100%, ${prefersDark ? 0 : 100}%)`,
+    };
 
     const [selectedStop, setSelectedStop] = useState<string | undefined>();
     const activeIsochrone = selectedStop ? isochrones[selectedStop] : undefined;
     return (
       <div className="w-full h-full">
-        <MapContainer center={center} zoom={zoom} scrollWheelZoom={true} preferCanvas={true}>
+        <MapContainer center={center} zoom={zoom} scrollWheelZoom={true} preferCanvas={false}>
           <VectorBasemapLayer
             // todo: Possible to overlay labels and road markings over geojson?
             styleKey={prefersDark ? "ArcGIS:DarkGray" : "ArcGIS:LightGray"}
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             apiKey={process.env.NEXT_PUBLIC_ESRI_API_KEY!}
           />
-          {activeIsochrone && <GeoJSON key={selectedStop} data={activeIsochrone} />}
+          {activeIsochrone && (
+            <GeoJSON
+              key={selectedStop}
+              data={activeIsochrone}
+              style={(feature) => {
+                return {
+                  // TODO: make typescript happy again
+                  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                  // @ts-expect-error
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+                  fillColor: durationColors[feature.properties.duration],
+                  fillOpacity: 0.5,
+                  color: prefersDark ? "black" : "white",
+                  weight: prefersDark ? 1 : 2,
+                };
+              }}
+            />
+          )}
           {stops.map((stop) => (
             <CircleMarker
               key={stop.stop_code}
               center={[stop.stop_lat, stop.stop_lon]}
               pathOptions={{
                 color: "black",
-                fillOpacity: 1,
+                fillOpacity: prefersDark ? 0.8 : 1,
                 fillColor: selectedStop == stop.stop_id ? "yellow" : "white",
                 weight: 1,
               }}
