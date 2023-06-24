3b799a54946822d9990c1182f12bed694ff2d43f
Rob Jackson <rob@rjackson.me>
2023-06-24 15:40:56 +0100

    Better metrolink lines

--- pages/time.tsx
+++ pages/time.tsx
@@ -1,25 +1,21 @@
 import { usePrefersDark } from "@rjackson/rjds";
 import "leaflet/dist/leaflet.css";
 import dynamic from "next/dynamic";
-import { FeatureGroup, LatLngExpression, LatLngTuple } from "leaflet";
+import { FeatureGroup, LatLngTuple } from "leaflet";
 import Head from "next/head";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { loadIsochrones } from "../lib/isochrones";
 import { ReactElement, RefObject, useEffect, useRef, useState } from "react";
-import { emerald, fuchsia, indigo, pink, purple, red, sky, yellow } from "tailwindcss/colors";
+import { fuchsia, indigo, yellow } from "tailwindcss/colors";
 import { MapLayout } from "../components/layouts/MapLayout";
 import useResizeObserver from "@react-hook/resize-observer";
 import stops from "../lib/gtfs-stops";
-import lines from "../lib/gtfs-lines";
 
 import type { LayerSpecification, StyleSpecification } from "maplibre-gl";
+import { loadMetrolinkLinesGeoJSON } from "../lib/tfgm-open-data/gm-metrolink-network/loadMetrolinkLines";
 
-type MapProps = {
-  isochrones: Awaited<ReturnType<typeof loadIsochrones>>;
-};
-
-const initClientOnlyMap = async (): Promise<(props: MapProps) => JSX.Element> => {
-  const { MapContainer, GeoJSON, CircleMarker, useMap, FeatureGroup, Polyline, Pane } = await import("react-leaflet");
+const initClientOnlyMap = async (): Promise<(props: InferGetStaticPropsType<typeof getStaticProps>) => JSX.Element> => {
+  const { MapContainer, GeoJSON, CircleMarker, useMap, FeatureGroup, Pane } = await import("react-leaflet");
   const { VectorBasemapLayer } = await import("../components/leaflet/VectorBasemapLayer");
 
   const center: LatLngTuple = [53.4781, -2.2433]; // St Peters Square
@@ -43,7 +39,7 @@ const initClientOnlyMap = async (): Promise<(props: MapProps) => JSX.Element> =>
     return null;
   };
 
-  const ClientOnlyMap = ({ isochrones }: MapProps) => {
+  const ClientOnlyMap = ({ isochrones, lines }: InferGetStaticPropsType<typeof getStaticProps>) => {
     const prefersDark = usePrefersDark();
     const containerRef = useRef<HTMLDivElement>(null);
     const mapRef = useRef<L.Map>(null);
@@ -55,16 +51,6 @@ const initClientOnlyMap = async (): Promise<(props: MapProps) => JSX.Element> =>
       "20": prefersDark ? fuchsia[500] : indigo[600],
       "30": prefersDark ? fuchsia[700] : indigo[400],
       "40": prefersDark ? fuchsia[900] : indigo[200],
-    };
-
-    const lineColors = {
-      "Blue Line": sky[500],
-      "Green Line": emerald[500],
-      "Navy Line": indigo[500],
-      "Pink Line": pink[500],
-      "Purple Line": purple[500],
-      "Red Line": red[500],
-      "Yellow Line": yellow[500],
     };
 
     // We need to separate vector maps into 2 layers so we can display part of it as the basemap, and more detailed
@@ -157,11 +143,11 @@ const initClientOnlyMap = async (): Promise<(props: MapProps) => JSX.Element> =>
 
     const [selectedStop, setSelectedStop] = useState<string | undefined>();
     const activeIsochrone = selectedStop ? isochrones[selectedStop] : undefined;
-    const outboundLines = lines.filter((line) => line.route_id.endsWith(":O:CURRENT"));
 
     useResizeObserver(containerRef, () => {
       mapRef.current?.invalidateSize();
     });
+
     return (
       <div ref={containerRef} className="w-full h-full">
         <MapContainer ref={mapRef} center={center} zoom={zoom} scrollWheelZoom={true} preferCanvas={false}>
@@ -249,21 +235,22 @@ const initClientOnlyMap = async (): Promise<(props: MapProps) => JSX.Element> =>
             />
           )}
           <FeatureGroup ref={linesRef}>
-            {outboundLines.map((line) => (
-              <Polyline
-                key={line.route_id}
-                positions={line.stops.map(({ stop_lat, stop_lon }): LatLngExpression => [stop_lat, stop_lon])}
-                pathOptions={{
-                  // TODO: make typescript happy again
-                  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                  // @ts-expect-error
-                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-                  color: lineColors[line.route_short_name],
-                  opacity: 1,
-                  weight: 2,
-                }}
-              />
-            ))}
+            {!prefersDark && <GeoJSON
+              data={lines}
+              pathOptions={{
+                color: yellow[500],
+                opacity: 1,
+                weight: 5,
+              }}
+            />}
+            <GeoJSON
+              data={lines}
+              pathOptions={{
+                color: prefersDark ? yellow[300] : yellow[300],
+                opacity: 1,
+                weight: 3,
+              }}
+            />
           </FeatureGroup>
 
           <FeatureGroup ref={markersRef}>
@@ -321,7 +308,7 @@ const initClientOnlyMap = async (): Promise<(props: MapProps) => JSX.Element> =>
 
 const IsochroneMap = dynamic(initClientOnlyMap, { ssr: false });
 
-export default function TravelTimeMap({ isochrones }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function TravelTimeMap({ isochrones, lines }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <Head>
@@ -332,7 +319,7 @@ export default function TravelTimeMap({ isochrones }: InferGetStaticPropsType<ty
           <H3 id="travel-time-map">Travel Time Map</H3>
         </Section> */}
 
-      <IsochroneMap isochrones={isochrones} />
+      <IsochroneMap isochrones={isochrones} lines={lines} />
       {/* </div> */}
     </>
   );
@@ -340,13 +327,16 @@ export default function TravelTimeMap({ isochrones }: InferGetStaticPropsType<ty
 
 export const getStaticProps: GetStaticProps<{
   isochrones: Awaited<ReturnType<typeof loadIsochrones>>;
+  lines: Awaited<ReturnType<typeof loadMetrolinkLinesGeoJSON>>;
 }> = async () => {
   // TODO: Expose this publically and lazy load, rather than shove a ton of data right down the pipe?
   const isochrones = await loadIsochrones();
+  const lines = await loadMetrolinkLinesGeoJSON();
 
   return {
     props: {
       isochrones,
+      lines,
     },
   };
 };
